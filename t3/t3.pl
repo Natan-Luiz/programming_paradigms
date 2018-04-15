554c4328597b62288be59ee1206a8f546f3cd524
Natan-Luiz <37088299+Natan-Luiz@users.noreply.github.com>
2018-04-15 01:55:01 -0300

    Update t3.pl

--- t3/t3.pl
+++ t3/t3.pl
@@ -17,3 +17,29 @@ has5(L):-
 hasN(L,N) :-
     length(L,N).
     
+% Q4
+
+
+% Q5
+
+zipmult([],[],[]).
+zipmult(L1,L2,L3) :-
+    L1 = [H1|T1],
+    L2 = [H2|T2],
+    H3 is H1*H2,
+    zipmult(T1,T2,Z),
+    L3 = [H3|Z].
+
+% Q6
+
+
+% Q7
+
+
+% Q8
+
+
+% Q9
+
+
+% Q10
