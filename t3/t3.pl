9cf7d27f2585b38a23574eae19af617b6c9b1af0
Natan-Luiz <37088299+Natan-Luiz@users.noreply.github.com>
2018-04-15 01:49:26 -0300

    Update t3.pl

--- t3/t3.pl
+++ t3/t3.pl
@@ -1,3 +1,19 @@
 % PARADIGMAS DA PROGRAMAÇÃO
 % TRABALHO 3
 % NATAN LUIZ PAETZHOLD BERWALDT
+
+% Q1
+
+zeroInit(L) :-
+    L = [0|_].
+
+% Q2
+
+has5(L):-
+    L = [_,_,_,_,_].
+
+% Q3
+
+hasN(L,N) :-
+    length(L,N).
+    
