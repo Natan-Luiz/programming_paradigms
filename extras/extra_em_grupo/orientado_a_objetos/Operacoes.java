0e6d4ac7d1045474d784d637b54c6a4278bca5e6
Natan-Luiz <37088299+Natan-Luiz@users.noreply.github.com>
2018-05-02 22:07:10 -0300

    Create Operacoes.java

---
+++ extras/extra_em_grupo/orientado_a_objetos/Operacoes.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package orientadoaobjetos;
+
+/**
+ *
+ * @author André L. Rakowski
+ */
+public class Operacoes {
+
+    private void deslizaControle(Dados dados, Integer inicio, Integer fim) {
+        if (inicio <= fim) {
+            for (int i = inicio; i < fim; i++) {
+                dados.aparicoes[dados.barra[i]] += 1;
+            }
+        } else {
+            for (int i = inicio; i > fim; i--) {
+                dados.aparicoes[dados.barra[i]] += 1;
+            }
+        }
+    }
+
+    public void calculaAparicoes(Dados dados) {
+        for (int i = 0; i < dados.m - 1; i++) {
+            deslizaControle(dados, dados.sequencia[i] - 1, dados.sequencia[i + 1] - 1);
+        }
+        dados.aparicoes[dados.barra[dados.sequencia[dados.m - 2 + 1] - 1]] += 1;
+    }
+
+}
