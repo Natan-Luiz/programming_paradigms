c3544ba241cff730bc231529b1bb5de6daedae46
Natan-Luiz <37088299+Natan-Luiz@users.noreply.github.com>
2018-05-04 11:08:33 -0300

    Update medalhas.hs

--- extras/medalhas/medalhas.hs
+++ extras/medalhas/medalhas.hs
@@ -1,6 +1,8 @@
 {-  
   Medalhas
 
+https://olimpiada.ic.unicamp.br/pratique/p1/2016/f2/medalhas/
+
 A natação foi um dos esportes mais emocionantes das Olimpíadas do Rio. Houve até uma prova na qual três atletas chegaram empatados, cada um recebendo uma medalha de prata! Normalmente, porém, os três primeiros colocados terminam a prova em tempos distintos e, portanto, temos a distribuição mais comum de medalhas: o nadador que terminou no menor tempo recebe medalha de ouro; o nadador que terminou com o segundo menor tempo recebe medalha de prata; e o que terminou com o terceiro menor tempo recebe medalha de bronze. Neste problema, dados os três tempos distintos de finalização da prova, dos três nadadores que ganharam medalhas, seu programa deve dizer quem ganhou medalha de ouro, quem ganhou prata e quem ganhou bronze.
 Entrada
 A primeira linha da entrada contém um inteiro T1, indicando o tempo em que o nadador 1 terminou a prova. A segunda linha da entrada contém um inteiro T2, indicando o tempo de finalização do nadador 2. Por fim, a terceira linha da entrada contém um inteiro T3, indicando o tempo em que o nadador 3 terminou a prova.
