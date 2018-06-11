9da67c273abeb0b6546bb11fba856c0bd2881de3
Natan-Luiz <37088299+Natan-Luiz@users.noreply.github.com>
2018-06-11 18:09:08 -0300

    Create GraphMaker.java

---
+++ t6/GraphMaker.java
@@ -0,0 +1,54 @@
+
+package myplanarity;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+
+public final class GraphMaker {
+    Grafo ALL;
+    public GraphMaker(int verts, Grafo g){
+        ALL = g;
+        int cont = 3;
+        newTriangle();
+        while(verts >= cont){
+            makeTriangle(cont);
+            cont++;
+        }
+    }
+    public void makeTriangle(int cont){
+        ALL.addVertice(newCircle());
+        
+        ALL.addAresta( newLine(), ALL.getVertice(cont), ALL.getVertice(cont-1));
+        ALL.addAresta( newLine(), ALL.getVertice(cont), ALL.getVertice(cont-2));
+        
+    }
+    public void newTriangle(){
+        ALL.addVertice(newCircle());
+        ALL.addVertice(newCircle());
+        ALL.addVertice(newCircle());
+        
+        ALL.addAresta( newLine(), ALL.getVertice(0), ALL.getVertice(1));
+        ALL.addAresta( newLine(), ALL.getVertice(1), ALL.getVertice(2));
+        ALL.addAresta( newLine(), ALL.getVertice(2), ALL.getVertice(0));
+    }
+    
+    public Circle newCircle(){
+        Circle c = new Circle(0,0,12,Color.DARKRED);
+        
+        c.setStrokeWidth(5);
+        c.setStroke(Color.DARKRED);
+        
+        return c;
+    }
+    
+    public Line newLine(){
+        Line l = new Line(0,0,0,0);
+        
+        l.setStrokeWidth(3);
+        l.setStroke(Color.CORAL);
+        
+        return l;
+    }
+    
+}
